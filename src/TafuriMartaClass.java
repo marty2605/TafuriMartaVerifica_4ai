13eece42e326123436af0949fb50dde5cfb93537
TafuriMarta <martataf26@gmail.com>
2023-02-04 10:56:06 +0100

    versione 0: errata

---
+++ src/TafuriMartaClass.java
@@ -0,0 +1,43 @@
+/**
+ * <U>classe produttoria</U> <i>04/02/2023</i>
+ * @author <b>Tafuri Marta</b>
+ * 
+ *
+ */
+public class TafuriMartaClass {
+	/**
+	 * @param s indice iniziale
+	 * @param f indice finale
+	 */
+	private int s,f;
+	
+	public TafuriMartaClass(int s, int f) {
+		this.s = s;
+		this.f = f;
+	}
+	/**
+	 * 
+	 * @param s indice iniziale
+	 * @param f indice finale
+	 * @return r moltiplicazione di fattori
+	 */
+	public int TafuriMet(int s,int f) {
+		int r=0;
+		for(int i=1;i<f;i++) {
+			r=r+r;
+		}
+		return r;
+	}
+	
+	public String toString() {
+		String st="indice iniziale = "+s+"\nindice finale = "+f+"\n\nmoltiplicazione dei fattori: "+TafuriMet(s,f);
+		return st;
+	}
+
+	public static void main(String[] args) {
+		TafuriMartaClass t=new TafuriMartaClass(5,7);
+		t.TafuriMet(5, 7);
+		System.out.println(t);
+	}
+
+}
